bfbfeacc5ef0453fd974d1c3e6c0d7e98e568656
kiemaBruce <brucekiema292@gmail.com>
2023-12-04 10:31:29 +0300

    Repetition Token #3

---
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+if !ARGV.empty?
+	capt = ARGV[0]
+	match_arr = capt.scan(/hbt*n/)
+	if match_arr.length > 0
+		r_str = match_arr.join("\n")
+		puts r_str
+	else
+		exit
+	end
+else
+	exit
+end
