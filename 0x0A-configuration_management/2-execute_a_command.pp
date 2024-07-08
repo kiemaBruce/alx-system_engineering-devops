cb1317a6edf838aec2289f0b76aa19691aa78ba3
kiemaBruce <brucekiema292@gmail.com>
2024-07-08 19:20:35 +0300

    Remove check for whether process is running before terminating it

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # kills a process named killmenow
 exec { 'kill_killmenow':
   command => '/usr/bin/pkill killmenow',
-  onlyif  => '/usr/bin/pgrep killmenow'
+  #onlyif  => '/usr/bin/pgrep killmenow'
 }
