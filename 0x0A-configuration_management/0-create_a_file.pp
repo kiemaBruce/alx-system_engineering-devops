15ff9e44c5cc7a17c8cc982707a5bb1064e389a9
kiemaBruce <brucekiema292@gmail.com>
2024-07-08 18:23:03 +0300

    Insert colon that was missing, correct linter errors

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # Creates a file, grants permissions, assigns group and owner, and writes to it.
-file { '/tmp/school'
-  ensure => file,
-  mode => '0744',
-  owner => 'www-data',
-  group => 'www-data',
+file { '/tmp/school':
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
   content => 'I love Puppet'
 }
